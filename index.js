cd38156483ccc2a9c377ed574b35dd55021a69c1
seknd <76913607+seknd@users.noreply.github.com>
2023-08-25 00:54:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,93 @@
 
 
-    let score = JSON.parse(localStorage.getItem('score')) || {
-        heads: 0,
-    tails: 0,
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    looses: 0,
+    ties: 0,
+};
+
+updateScoreElement()
+
+function playGame(playerMove) {
+
+    let computerMove = pickComputerMove();
+
+    let res = '';
+
+
+    if (playerMove === 'Rock') {
+        if (computerMove === 'rock') {
+            res = 'Tie.';
+        } else if (computerMove === 'paper') {
+            res = 'You lose.';
+        } else if (computerMove === 'scissors') {
+            res = 'You win.';
+        }
+    }
+
+    else if (playerMove === 'Paper') {
+        if (computerMove === 'rock') {
+            res = 'You win.';
+        } else if (computerMove === 'paper') {
+            res = 'Tie.'; 
+        } else if (computerMove === 'scissors') {
+            res = 'You loose.';
+        }
+    }
+
+    else if (playerMove === 'Scissors') {
+        if (computerMove === 'rock') {
+            res = 'You loose.';
+        } else if (computerMove === 'paper') {
+            res = 'You win.';
+        } else if (computerMove === 'scissors') {
+            res = 'Tie.';
+        }
+    }
+
+    if (res === 'You win.') {
+        score.wins += 1;
+    } else if (res === 'You lose.') {
+        score.looses += 1;
+    } else if (res === 'Tie.') {
+        score.ties += 1;
     }
 
 
 
-    function flip2() {
+    localStorage.setItem('score', JSON.stringify(score));
 
-        //console.log(localStorage.getItem('score'))
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, looses:${score.looses}, ties:${score.ties}`
+    document.querySelector('.js-res').innerHTML = `You picked ${playerMove}. Computerpicked ${computerMove}. ${res} `
+    /* alert(`You picked ${playerMove}. Computerpicked ${computerMove}. ${res} 
+Wins: ${score.wins}, looses:${score.looses}, ties:${score.ties}`
+   );*/
+}
 
-        let flip = Math.random();
-    let res = ''
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, looses:${score.looses}, ties:${score.ties}`
+}
 
-    console.log(flip);
-       
-        if (flip > 0.5 ) {
-        res = 'Head'
-        score.heads += 1
-        document.querySelector('.js-coin').innerHTML = `H`
-            }else if (flip < 0.5) {
-                 res = 'Tails'
-                 score.tails += 1
-                document.querySelector('.js-coin').innerHTML = `T`
-                   }
+function resoult() {
+    document.querySelector('.js-res').innerHTML = `You picked ${playerMove}. Computerpicked ${computerMove}. ${res} `
+}
 
-        
-        
-       
-         localStorage.setItem('score',JSON.stringify(score))
 
-        document.querySelector('.js-got').innerHTML = `You filped coin and got ${res}`
-        document.querySelector('.js-res').innerHTML = `Your score is: ${score.heads} Heads and ${score.tails} tails`;
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3) {
+        computerMove = 'scissors';
     }
 
-    
-//please dont hate me for this ugli code thanks <3
+    return computerMove;
+}
 
-   
+//please dont hate me for this ugly code
